9686381fef6a6deb6f17db0d38ee19e3c7c516f6
unknown <dmthang.dhti15a12hn@sv.uneti.edu.vn>
2024-07-12 03:43:45 +0700

    Viết API lấy danh sách bài post

--- src/app/post/api/route.ts
+++ src/app/post/api/route.ts
@@ -50,3 +50,38 @@ export async function POST(req:NextRequest){
         )
       }
 }
+export async function GET(req:NextRequest){
+    await ConnectDB();
+    // lấy ra limit
+    const limit = req.nextUrl.searchParams.get("limit") ?? 2;
+    // lấy ra page
+    const page = req.nextUrl.searchParams.get("page") ?? 1;
+    // yêu cầu từ phía backend gửi về số bài post
+    const totalpost = await Post.countDocuments();
+    // yêu cầu từ phía backend gửi về số trang post
+    const totalpage = Math.ceil(totalpost / +limit);
+    // limit giởi hạn số phần tử trong mảng
+    const allPost = await Post.find().skip((+page - 1) * +limit).limit(+limit)
+    try {
+        return NextResponse.json({
+            data:allPost,
+            meta:{
+                Totalpage:totalpage,
+                TotalCount: totalpost 
+            },
+            message:"Success"
+        },{
+            status:201,
+            statusText:"Create"
+        })
+    } catch (error) {
+        console.log({error})
+        return NextResponse.json({
+            data:null,
+            message:"Failde"
+        },{
+            status:400,
+            statusText:"Error"
+        })
+    }
+}
